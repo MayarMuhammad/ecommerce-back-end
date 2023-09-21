06fdd2b057bdfcf81c7fddf3fad40ec5a5e9cccb
MayarMuhammad <mayarmuhammad465@gmail.com>
2023-09-21 21:07:04 +0300

    atlas commit

--- Database/connection.js
+++ Database/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   return await mongoose
-    .connect(process.env.DB_URL)
+    .connect(process.env.DB_URL_ATLAS)
     .then(() => {
       console.log("DB Connected");
     })
